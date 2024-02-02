7149a7ce8c13552df945b15e1487d68e5f4ef315
Justine Launay <jlaunay8@outlook.fr>
2024-02-02 17:41:20 +0100

    Vérification des champs

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,32 +1,22 @@
 //Variables globales pour login
+const loginForm = document.getElementById("login-form");
 const email = document.querySelector("form #email");
 const password = document.querySelector("form #password");
-const loginForm = document.getElementById("login-form");
-const errorMessage = document.querySelector(".login p");
 
-//Récupérer les valeurs du formulaire
+//Récupérer les valeurs du formulaire et les vérifier
+    //Ajout d'un listener
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+    //Récupérer et vérifier le champ password
     const userEmail = email.value;
     const userPasseword = password.value;
-    console.log(userEmail, userPasseword);
-});
-
-//Contrôler que le mail est saisi
-const emailVerified = document.getElementById("emailVerified");
-emailVerified.addEventListener ("input", function() {
-    const email = email = emailVerified.value;
-    const ValidEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+;
-    if (ValidEmail) {
-        emailVerified.setCustomValidity("");
-    } else {
-        emailVerified.setCustomValidity ("Mail non valide");
+        if (userPasseword ==="") {
+            window.alert("Le champ du mot de passe est vide");
+        }
+        console.log(userEmail, userPasseword);
     }
-});
-
-    //Si mail non saisi ou non conforme => Afficher message de'erreur
-    //Si password non saisi alors afficher message d'erreur
+    );
 
 //Contrôler que le password est saisi
 
@@ -35,4 +25,4 @@ emailVerified.addEventListener ("input", function() {
     //Sinon envoyer à l'API les données email et password
         //Si API envoie 200 => Rediriger vers page admin
             //Enregistrer le token
-fu        //Si API envoie erreur => Message erreur
+        //Si API envoie erreur => Message erreur

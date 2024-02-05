be0b60c0187d91267aff1bc4dfbac8e0c59c7e51
Justine Launay <jlaunay8@outlook.fr>
2024-02-05 19:43:10 +0100

    Fonction authentification (non fonctionnelle)

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -2,27 +2,39 @@
 const loginForm = document.getElementById("login-form");
 const email = document.querySelector("form #email");
 const password = document.querySelector("form #password");
-
 //Récupérer les valeurs du formulaire et les vérifier
     //Ajout d'un listener
     loginForm.addEventListener("submit", (e) => {
         e.preventDefault();
-
-    //Récupérer et vérifier le champ password
     const userEmail = email.value;
-    const userPasseword = password.value;
-        if (userPasseword ==="") {
+    const userPassword = password.value;
+    //Récupérer et vérifier le champ password
+        if (userPassword ==="") {
             window.alert("Le champ du mot de passe est vide");
         }
-        console.log(userEmail, userPasseword);
-    }
-    );
-
-//Contrôler que le password est saisi
-
-    //Si erreur dans le formulaire (saisie incorrecte) => Stop code
-
-    //Sinon envoyer à l'API les données email et password
-        //Si API envoie 200 => Rediriger vers page admin
-            //Enregistrer le token
-        //Si API envoie erreur => Message erreur
+        console.log(userEmail, userPassword);
+    });
+// Envoie une requête POST à l'URL de l'API avec les informations d'authentification
+async function authentification (email, password) {
+    const responseLoginAPI = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email,
+          password
+        })
+    })
+    // Enregistrer le token
+    if (responseLoginAPI.status === 200) {
+        const data = await responseLoginAPI.json();
+        const token = token.value;
+        window.localStorage.setItem("token", token);
+        console.log ("Jeton enregistré:", token);
+        return data;
+    // Rediriger vers page admin
+    } else {
+        window.alert("L'identifiant ou le mot de passe est incorrect");
+      }
+};

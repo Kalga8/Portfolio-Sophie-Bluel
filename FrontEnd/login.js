7c605b20f7377549e4667105121b658142e33a3f
Justine Launay <jlaunay8@outlook.fr>
2024-02-06 18:55:15 +0100

    Authentification ok

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -5,43 +5,44 @@ const password = document.querySelector("form #password");
 const urlLoginApi = "http://localhost:5678/api/users/login";
 
 //Récupérer les valeurs du formulaire et les vérifier
-    //Ajout d'un listener
-    loginForm.addEventListener("submit", (e) => {
-        e.preventDefault();
+  //Ajout d'un listener
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
     const userEmail = email.value;
     const userPassword = password.value;
     //Récupérer et vérifier le champ password
-        if (userPassword ==="") {
-            window.alert("Le champ du mot de passe est vide");
-        }
-        console.log(userEmail, userPassword);
-    });
+      if (userPassword ==="") {
+        window.alert("Le champ du mot de passe est vide");
+    }
+    authentification(userEmail, userPassword);
+  });
+
 // Envoie une requête POST à l'URL de l'API avec les informations d'authentification
-async function authentification (email, password) {
+async function authentification(email, password) {
   try {
     const responseLoginAPI = await fetch(urlLoginApi, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          email,
-          password
-        })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
     });
     // Enregistrer le token
     if (responseLoginAPI.status === 200) {
-        const data = await responseLoginAPI.json();
-        const token = data.token;
-        window.localStorage.setItem("token", token);
-        console.log ("Jeton enregistré:", token);
-        return data;
-    // Rediriger vers page admin
+      const data = await responseLoginAPI.json();
+      const token = data.token;
+      window.localStorage.setItem("token", token);
+      console.log("Jeton enregistré:", token);
+      window.location.href = "./index.html";
+      return data;
+      // Rediriger vers page admin
     } else {
-        window.alert("L'identifiant ou le mot de passe est incorrect");
-      }
+      window.alert("L'identifiant ou le mot de passe est incorrect");
+    }
   } catch (error) {
     console.error("Erreur lors de la connexion à l'API :", error);
   }
-};
-authentification();
+}

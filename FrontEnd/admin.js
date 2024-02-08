142077c3605634295e2a81c5ed9f83e7803cd203
Justine Launay <jlaunay8@outlook.fr>
2024-02-08 15:16:53 +0100

    Disparition filtres & apparition bouton modifier projets

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -7,13 +7,14 @@ async function interfaceAdmin() {
         //Afficher bandeau noir mode édition
             const banner = document.querySelector(".banner-admin");
             banner.style.display = "flex";
-
         //Lien modification projets
-
+        const iconModification = document.querySelector(".modification-project");
+        iconModification.style.display ="flex";
         //Enlever filtres
+        const filters = document.querySelector(".filter-bar");
+        filters.style.display = "none";
       }
     });
   }
-  
   interfaceAdmin();
       

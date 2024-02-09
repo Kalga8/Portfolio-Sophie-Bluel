615e40a4a0618e0473c8926eddf2ab24067aadf3
Justine Launay <jlaunay8@outlook.fr>
2024-02-09 13:33:50 +0100

    Mentorat

--- FrontEnd/modales.js
+++ FrontEnd/modales.js
@@ -3,9 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
 //Affichage de la modale
 const modification = document.querySelector (".modification-project");
 const modalWindow = document.querySelector (".modal-window");
+const closeModal = document.querySelector (".xmark");
     //Ajout eventListener sur modifier
     modification.addEventListener("click", () => {
         modalWindow.style.display = "flex";
+    });
+    closeModal.addEventListener("click", () => {
+        modalWindow.style.display = "none";
     });
 }
 )};

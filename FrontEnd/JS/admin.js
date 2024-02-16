578c456d5c2360be952cbb950e53bb63860fc5a4
Justine Launay <jlaunay8@outlook.fr>
2024-02-16 10:35:35 +0100

    Création JS Bandeau noir admin

--- FrontEnd/JS/admin.js
+++ FrontEnd/JS/admin.js
@@ -1,15 +1,29 @@
 async function interfaceAdmin() {
     document.addEventListener("DOMContentLoaded", function () {
-        //Changer login en logout
         const logout = document.querySelector("header nav ul li:nth-child(3) a");
+        const banner = document.querySelector("header");
+        //Changer login en logout
         if (localStorage.loged == "true") {
             logout.textContent = "logout";
         //Afficher bandeau noir mode édition
-            const banner = document.querySelector(".banner-admin");
-            banner.style.display = "flex";
+            const bannerContainer = document.createElement("div");
+            bannerContainer.className = "banner-admin";
+            banner.appendChild(bannerContainer);
+            banner.insertBefore(bannerContainer, banner.firstChild);
+
+            const penIcon = document.createElement("img");
+            penIcon.src = "./assets/icons/pen-to-square-regular-white.svg";
+            penIcon.className = "pen-admin";
+            bannerContainer.appendChild(penIcon);
+
+            const edition = document.createElement("p");
+            edition.textContent = "Mode édition";
+            bannerContainer.appendChild(edition);
+
         //Lien modification projets
         const iconModification = document.querySelector(".modification-project");
         iconModification.style.display ="flex";
+
         //Enlever filtres
         const filters = document.querySelector(".filter-bar");
         filters.remove();
